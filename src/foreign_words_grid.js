f9378bc4cd8b040bc4000a872edc15eaee46c9d2
Ganesh <ganeshmaha@users.noreply.github.com>
2022-07-04 20:02:08 +0530

    Add files via upload

---
+++ src/foreign_words_grid.js
@@ -0,0 +1,29 @@
+    new gridjs.Grid({
+    columns: [
+	 {
+        id: 'fw',
+        name: gridjs.html('Foreign Word')
+      },
+ 	 {
+        id: 'tw',
+        name: gridjs.html('Tamil Word')
+      }
+	],
+	pagination: {
+		limit:20
+	},
+	server: {
+    	url: 'manavai_words_list.json',
+    	then: data => data.map(card => [card.fw, card.tw]),
+    	handle: (res) => {
+      // no matching records found
+      if (res.status === 404) return {data: []};
+      if (res.ok) return res.json();
+      throw Error('oh no :(');
+    	}
+	},
+	search: {
+    	enabled: true
+  	}
+,
+  }).render(document.getElementById("wrapper"));
